fab3c96af0db4b3ff98b9bbbf15ba29a7f2d6b00
Philippe Renard <philippe.renard@unine.ch>
2018-01-13 18:23:12 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='geostats',
+setup(name='geostat',
       version='0.1',
       description='A basic geostatistical package',
       url='https://github.com/UniNE-CHYN/geostat.git',
