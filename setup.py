266ddf82e8f635a351e72012157ad85d59d82daa
Philippe Renard <philippe.renard@unine.ch>
2024-09-08 10:37:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='geostat',
       version='0.1',
       description='A basic geostatistical package',
-      url='https://github.com/UniNE-CHYN/geostat.git',
+      url='https://github.com/randlab/geostat.git',
       author='Philippe Renard',
       author_email='philippe.renard@unine.ch',
       license='MIT',
